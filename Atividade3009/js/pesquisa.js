f699f3a8bba92337b7f9a2e5f066ad42edc4c4fe
Lucas <lucas.roseira@fatec.sp.gov.br>
2020-09-30 23:23:27 -0300

    Atualização

--- Atividade3009/js/pesquisa.js
+++ Atividade3009/js/pesquisa.js
@@ -1,25 +1,47 @@
 var quantidade = 0;
 var arrayIdade = [];
+var totalIdades = 0;
 
 function Enviar() {
 
 	var idade = document.getElementById("idade");
 	var sexo = document.getElementById("sexo");
 	var opiniao = document.getElementById("opiniao");
-
 	var maiorIdade;
+	var menorIdade;
+	var media;
+	var qtdPessimo;
+	var porcentagemPessimoBom;
 	var i;
 
 	quantidade++;
+	totalIdades += idade.value;
+
+	if(opiniao.value == "pessimo") {
+		qtdPessimo++;
+	}
+
+	alert('Total Idades: ' + totalIdades);
 
 	arrayIdade.push(idade.value);
+	alert('Pesquisa enviada com sucesso!');
 
 
 	if(quantidade == 3) {
 
 		maiorIdade = Math.max.apply(null, arrayIdade);
-		return alert('Maior idade: ' + maiorIdade);
-	}
+		alert('Maior idade: ' + maiorIdade);
 
+		menorIdade = Math.min.apply(null, arrayIdade);
+		alert('Menor idade: ' + menorIdade);
+
+		media = Math.round(totalIdades / 3);
+		alert('Media Idades: ' + media);
+
+		alert('Quantidade de Péssimo: ' + qtdPessimo);
+
+		window.reload();
+
+	}
 
 }
